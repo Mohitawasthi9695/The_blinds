816f2d70f982c903b7aceb6bcebfa44526805860
Mohit Awasthi <mohitawasthi9695@gmail.com>
2025-01-25 12:57:15 +0530

    changes

---
+++ app/Http/Controllers/WarehouseAccessoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WarehouseAccessory;
+use Illuminate\Http\Request;
+
+class WarehouseAccessoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WarehouseAccessory $warehouseAccessory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WarehouseAccessory $warehouseAccessory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WarehouseAccessory $warehouseAccessory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WarehouseAccessory $warehouseAccessory)
+    {
+        //
+    }
+}
---
+++ app/Models/WarehouseAccessory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+
+class WarehouseAccessory extends Model
+{
+    use HasFactory,HasApiTokens;
+    protected $guarded = [];
+
+}
